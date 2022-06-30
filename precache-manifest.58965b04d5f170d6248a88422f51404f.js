5b0c65f702c18726837df5d08bad700264f964bc
maasiimka <mrozisalovec@gmail.com>
2022-06-30 15:39:33 +0300

    Updates

---
+++ precache-manifest.58965b04d5f170d6248a88422f51404f.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e0b9d2f0e8c59dbfbba",
+    "url": "/phonebook/static/css/main.e2d9abf9.chunk.css"
+  },
+  {
+    "revision": "3e0b9d2f0e8c59dbfbba",
+    "url": "/phonebook/static/js/main.3e0b9d2f.chunk.js"
+  },
+  {
+    "revision": "3f109043bcb48f61ff00",
+    "url": "/phonebook/static/css/home-page.b02fe9b6.chunk.css"
+  },
+  {
+    "revision": "3f109043bcb48f61ff00",
+    "url": "/phonebook/static/js/home-page.3f109043.chunk.js"
+  },
+  {
+    "revision": "1a9b1ed971746c6db9c3",
+    "url": "/phonebook/static/css/phonebook.2421e193.chunk.css"
+  },
+  {
+    "revision": "1a9b1ed971746c6db9c3",
+    "url": "/phonebook/static/js/phonebook.1a9b1ed9.chunk.js"
+  },
+  {
+    "revision": "075f4b396132b517820a",
+    "url": "/phonebook/static/css/registration.60bf1acc.chunk.css"
+  },
+  {
+    "revision": "075f4b396132b517820a",
+    "url": "/phonebook/static/js/registration.075f4b39.chunk.js"
+  },
+  {
+    "revision": "9b83fac2c4acf7525ce9",
+    "url": "/phonebook/static/css/login.60bf1acc.chunk.css"
+  },
+  {
+    "revision": "9b83fac2c4acf7525ce9",
+    "url": "/phonebook/static/js/login.9b83fac2.chunk.js"
+  },
+  {
+    "revision": "b0d3286399007ddffb18",
+    "url": "/phonebook/static/js/5.b0d32863.chunk.js"
+  },
+  {
+    "revision": "cc564d42222d5216867d",
+    "url": "/phonebook/static/js/runtime~main.cc564d42.js"
+  },
+  {
+    "revision": "1f38a795d0f2c3591aed4e4ac7f67a77",
+    "url": "/phonebook/static/media/default-avatar.1f38a795.jpg"
+  },
+  {
+    "revision": "ab7d3cd1c3cc3ea9748684ec79274962",
+    "url": "/phonebook/static/media/deleteIcon.ab7d3cd1.svg"
+  },
+  {
+    "revision": "1c0e15b3908f8ac37acd1a6ecef54e39",
+    "url": "/phonebook/static/media/changeIcon.1c0e15b3.svg"
+  },
+  {
+    "revision": "8d7e1be5f1626531954ee860befcb2b2",
+    "url": "/phonebook/static/media/addIcon.8d7e1be5.svg"
+  },
+  {
+    "revision": "07a2906f638b4b089a44fc58d2b58155",
+    "url": "/phonebook/index.html"
+  }
+];
