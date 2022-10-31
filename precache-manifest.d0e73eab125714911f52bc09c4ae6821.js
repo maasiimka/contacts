6656fec512a12691029ed2293072d52ad9d2a0fb
maasiimka <mrozisalovec@gmail.com>
2022-10-31 16:12:39 +0200

    Updates

---
+++ precache-manifest.d0e73eab125714911f52bc09c4ae6821.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "55232ec7e540a78710cc",
+    "url": "/homepage/static/css/main.af2b6611.chunk.css"
+  },
+  {
+    "revision": "55232ec7e540a78710cc",
+    "url": "/homepage/static/js/main.55232ec7.chunk.js"
+  },
+  {
+    "revision": "43e1164f3d26b09c9433",
+    "url": "/homepage/static/css/home-page.2e3d35d4.chunk.css"
+  },
+  {
+    "revision": "43e1164f3d26b09c9433",
+    "url": "/homepage/static/js/home-page.43e1164f.chunk.js"
+  },
+  {
+    "revision": "3bc2f1ea9b74167fe2e7",
+    "url": "/homepage/static/css/phonebook.a693a8c7.chunk.css"
+  },
+  {
+    "revision": "3bc2f1ea9b74167fe2e7",
+    "url": "/homepage/static/js/phonebook.3bc2f1ea.chunk.js"
+  },
+  {
+    "revision": "d39e7d0cf214ce41979d",
+    "url": "/homepage/static/css/registration.b43f637b.chunk.css"
+  },
+  {
+    "revision": "d39e7d0cf214ce41979d",
+    "url": "/homepage/static/js/registration.d39e7d0c.chunk.js"
+  },
+  {
+    "revision": "0409e7d64ca4df7e2c87",
+    "url": "/homepage/static/css/login.b43f637b.chunk.css"
+  },
+  {
+    "revision": "0409e7d64ca4df7e2c87",
+    "url": "/homepage/static/js/login.0409e7d6.chunk.js"
+  },
+  {
+    "revision": "c5fc2bc7c932036ad376",
+    "url": "/homepage/static/js/5.c5fc2bc7.chunk.js"
+  },
+  {
+    "revision": "b049d6036a0ed3618c14",
+    "url": "/homepage/static/js/runtime~main.b049d603.js"
+  },
+  {
+    "revision": "cfcba127f1d81122bec071d045321d98",
+    "url": "/homepage/static/media/logo.cfcba127.svg"
+  },
+  {
+    "revision": "1f38a795d0f2c3591aed4e4ac7f67a77",
+    "url": "/homepage/static/media/default-avatar.1f38a795.jpg"
+  },
+  {
+    "revision": "ab7d3cd1c3cc3ea9748684ec79274962",
+    "url": "/homepage/static/media/deleteIcon.ab7d3cd1.svg"
+  },
+  {
+    "revision": "1c0e15b3908f8ac37acd1a6ecef54e39",
+    "url": "/homepage/static/media/changeIcon.1c0e15b3.svg"
+  },
+  {
+    "revision": "8d7e1be5f1626531954ee860befcb2b2",
+    "url": "/homepage/static/media/addIcon.8d7e1be5.svg"
+  },
+  {
+    "revision": "4dc4cf989980e3427ed0f37691d60c4f",
+    "url": "/homepage/static/media/preview.4dc4cf98.png"
+  },
+  {
+    "revision": "1a6fe9222b5bbb8dc2d6176c55681a50",
+    "url": "/homepage/static/media/example.1a6fe922.jpg"
+  },
+  {
+    "revision": "fae155791aef47c269d580d8e9b0002a",
+    "url": "/homepage/static/media/search.fae15579.svg"
+  },
+  {
+    "revision": "826413cf9ac4fa2af0d38bbe2762580e",
+    "url": "/homepage/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6d116b96ae25abb6830091614d3ddd85.js"
+  "/homepage/precache-manifest.d0e73eab125714911f52bc09c4ae6821.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/homepage/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
